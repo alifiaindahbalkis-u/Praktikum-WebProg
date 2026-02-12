e3360ac1abc9c53f17211a8aa58549e45ab1c299
alifia indah balkis <alifiaindahbalkis@gmail.com>
2026-02-12 21:23:46 +0700

    Initial upload portfolio website

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function showMessage() {
+  alert("Halo! Terima kasih sudah mengunjungi portofolio saya ^^");
+}
+
+// Scroll Animation
+const sections = document.querySelectorAll(".section");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+}, {
+  threshold: 0.2
+});
+
+sections.forEach(section => {
+  observer.observe(section);
+});
+
